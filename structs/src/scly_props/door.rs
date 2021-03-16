adc443d3af195aaaf65bf2155fe3b3ea17edbb71
Blake Spangenberg <bspangenberg@insteon.com>
2021-03-15 20:44:28 -0700

    .

--- structs/src/scly_props/door.rs
+++ structs/src/scly_props/door.rs
@@ -8,17 +8,19 @@ use crate::scly_props::structs::{ActorParameters, AncsProp};
 
 #[auto_struct(Readable, Writable)]
 #[derive(Debug, Clone)]
-pub struct Door<'r> {
+pub struct Door<'r>
+{
     #[auto_struct(expect = 14)]
     pub prop_count: u32,
 
     pub name: CStr<'r>,
+
     pub position: GenericArray<f32, U3>,
     pub rotation: GenericArray<f32, U3>,
     pub scale: GenericArray<f32, U3>,
 
     pub ancs: AncsProp,
-    pub actor_params: ActorParameters,//GenericArray<u32, U32>,
+    pub actor_params: ActorParameters,
 
     pub scan_offset: GenericArray<f32, U3>,
     pub collision_size: GenericArray<f32, U3>,
@@ -31,6 +33,7 @@ pub struct Door<'r> {
     pub is_morphball_door: u8,
 }
 
-impl<'r> SclyPropertyData for Door<'r> {
+impl<'r> SclyPropertyData for Door<'r>
+{
     const OBJECT_TYPE: u8 = 0x03;
 }
